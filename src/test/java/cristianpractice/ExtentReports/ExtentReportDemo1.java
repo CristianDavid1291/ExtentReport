852ba6f6b5d7ec37efc76b6de4b7920170262de2
Cristian David <cristiandavid1291@gmail.com>
2022-05-20 09:35:54 -0500

    src/test/java/cristianpractice/ExtentReports/ExtentReportDemo1.java documentation about Extent report

--- src/test/java/cristianpractice/ExtentReports/ExtentReportDemo1.java
+++ src/test/java/cristianpractice/ExtentReports/ExtentReportDemo1.java
@@ -17,8 +17,11 @@ public class ExtentReportDemo1 {
 	@BeforeTest
 	public void config() {
 		// ExtentReports, ExtentSparkReporter
+		//path to create our local html file
 		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		//this variable control the report settings
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		//Configuring our report settings
 		reporter.config().setReportName("Web Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
 		extent = new ExtentReports();
@@ -28,13 +31,15 @@ public class ExtentReportDemo1 {
 
 	@Test
 	public void initialDemo() {
-		
+		//Declaring variable test so as to control the specific test
 	    ExtentTest test =	extent.createTest("Initial Demo");
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
+		//we can report test fail manually
 		test.fail("Result do not match");
+		//Sending test results
 		extent.flush();
 	}
 
